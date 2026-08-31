22d45afc4630196db25ed9f4c6b8e7bda3347776
Yuri <am910021@gmail.com>
2026-08-31 17:11:48 +0000

    boards: archive G98 U-Boot board files

---
+++ boards/g98/u-boot/files/include/configs/g98-rk3588.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __G98_RK3588_H
+#define __G98_RK3588_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#include <configs/rk3588_common.h>
+
+#endif
